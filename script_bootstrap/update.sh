589552a140cbb8c2a2560e6ec147aa2634abb69d
iammrdm <iammrdm@proton.me>
2024-06-20 11:21:16 +0800

    Add update docker images

--- script_bootstrap/update.sh
+++ script_bootstrap/update.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+source $(dirname "$0")/function.sh
+# Base Directory: All Directories will be below this point
+DIRECTORY=pos
+DB_DATA=docker-datas
+BASE_DIRECTORY=/opt
+POS_DIRECTORY=$BASE_DIRECTORY/$DIRECTORY
+DB_DIRECTORY=$BASE_DIRECTORY/$DIRECTORY/$DB_DATA/pos-db
+
+
+echo "...running update"
+echo "...going to $POS_DIRECTORY"
+cd $POS_DIRECTORY
+
+echo "...pulling latest images"
+docker compose -f docker-compose-linux.yaml pull
+loading_icon 15 "...waiting to properly load new docker images"
+
+echo "...updating docker container to use new docker image"
+cd $POS_DIRECTORY
+docker compose -f docker-compose-linux.yaml up -d
+loading_icon 15 "...waiting containers to properly initialize"
+
+echo "...done updating the docker images"
+
